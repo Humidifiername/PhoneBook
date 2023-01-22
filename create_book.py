fe5a581f73347015dae113979c51bc3854af74bf
Albert <albert.fedorov.99@bk.ru>
2023-01-22 18:50:47 +0300

    Переделал функция создания телефонной книги

--- create_book.py
+++ create_book.py
@@ -7,13 +7,10 @@ contacts = [
     ["Roman", 89183434576],
     ["Igor", 89558342211]
 ]
- 
-with open(FILENAME, "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerows(contacts)
+
+def create_contacts(contacts):
+    with open(FILENAME, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(contacts)
      
- 
-# with open(FILENAME, "a", newline="") as file:
-#     user = ["Sam", 31]
-#     writer = csv.writer(file)
-#     writer.writerow(user)
+create_contacts(contacts)
