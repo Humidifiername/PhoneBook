0831a1fcd0af67959aa10d1a6a1afb6b0c024a19
Albert <albert.fedorov.99@bk.ru>
2023-01-23 19:52:46 +0300

    функция добавления нового контакта в телефонную книгу

--- add_new_contact.py
+++ add_new_contact.py
@@ -1,10 +1,9 @@
-import create_book as cb
+def add_contact():
+    name = input("Введите имя контакта: ")
+    phone = input("Введите телефон: ")
 
-name = input("Введите имя контакта: ")
-phone = input("Введите телефон: ")
+    new_contact = {name : phone}
 
-new_contact = {name : phone}
-
-with open(cb.FILENAME, 'a') as file:
-    for key, value in new_contact.items():
-        file.write(f'{key} - {value}\n')
+    with open("PhoneBook.txt", 'a') as file:
+        for key, value in new_contact.items():
+            file.write(f'{key} - {value}\n')
